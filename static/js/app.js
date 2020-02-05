4642c8a352cb10fd7b2a052063358f953b9acd9b
Eli Payne <epayne323@gmail.com>
2020-02-04 21:35:29 -0600

    added dropdown, barchart, metadata, & basic bubble

--- static/js/app.js
+++ static/js/app.js
@@ -4,15 +4,113 @@ d3.json("./samples.json").then(function(data)
     let metadata = data.metadata;
     let samples = data.samples;
 
-    // let dropdown = d3.select("#selDataset")
-    //     .selectAll("option")
-    //     .data(names)
-    //     .enter()
-    //     .append("option")
-    //     .attr("value", d => d.name)
-    //     .text(d => d.name);
+    let metatag = d3.select("#sample-metadata");
+    let bar = d3.select("#bar");
+    let gauge = d3.select("#gauge");
+    let bubble = d3.select("#bubble");
 
-    // console.log(dropdown);
+    // [9, 8, 7, ... 2, 1, 0]
+    let ten = Array.from(Array(10).keys()).reverse();
+    let colors = ["red", "orange", "yellow", "#bcbd22", "green", "#2ca02c", "blue", 
+        "indigo", "violet", "#e377c2"];
+
+    // populating the dropdown menu
+    let dropdown = d3.select("#selDataset");
+    dropdown.selectAll("option")
+        .data(names)
+        .enter()
+        .append("option")
+        .attr("value", d => d)
+        .text(d => d);
+
+
+    let dropdownChange = function()
+    {
+        // "this" will be whatever object calls the dropdownChange function
+        let newIndividual = d3.select(this).property("value");
+        let newSample = samples.filter(s => s.id === newIndividual);
+        let newMetadata = metadata.filter(s => s.id == newIndividual); // fuzzy match
+        
+        let sample_values = newSample[0].sample_values.slice(0,9);
+        let otu_id_strings = newSample[0].otu_ids.slice(0,9).map(x => `OTU ${x.toString()}`);
+
+
+        // demographic info
+        // remove existing list elements and append new metadata
+        metatag.selectAll("li").remove();
+        // the data we want is in an object with key "0"
+        Object.entries(newMetadata["0"]).forEach(([k,v]) =>
+        {
+            metatag.append("li")
+                .text(`${k}: ${v}`)
+                .attr(k);
+        });
+
+
+        // bar chart
+        bar.selectAll("div").remove();
+        // instead of using the otu ids directly, use descending 9 through 0 and then add ids as tick labels
+        let barTrace = 
+        {
+            x: sample_values,
+            y: ten,
+            type: "bar",
+            orientation: "h"
+        };
+        let barLayout = 
+        {
+            xaxis:
+            {
+                title: "Number of Samples"
+            },
+            yaxis: 
+            {
+                title: "Operational Taxonomic Unit ID",
+                tickmode: "array",
+                tickvals: ten,
+                ticktext: otu_id_strings
+            },
+        };
+        Plotly.newPlot("bar", [barTrace], barLayout);
+
+        // bubble chart
+        bubble.selectAll("div").remove();
+        bubbleTrace = 
+        {
+            x: otu_id_strings,
+            y: sample_values,
+            mode: 'markers',
+            marker: 
+            {
+                size: sample_values,
+                color: colors
+            }
+        };
+        bubbleLayout = 
+        {
+
+        };
+        Plotly.newPlot("bubble", [bubbleTrace], bubbleLayout);
+
+
+        // gauge.selectAll("div").remove();
+    };
+
+    // dropdownChange();
+    dropdown.on("change", dropdownChange);
+
+    let update = function(sample)
+    {
+
+    };
+
+
+    // let testSample = samples.filter(s => s.id === "978");
+
+    // console.log(testSample);
+        
+    
+    // var optionChanged = function(value) {return value;}
     // create the drop down menu of cities
 	// var selector = d3.select("body")
     // .append("select")
@@ -24,18 +122,4 @@ d3.json("./samples.json").then(function(data)
     // .attr("value", function (d, i) {
     //     return i;
     // });
-
-    // dropdown.append()
-
-    let trace1 = 
-    {
-
-    };
-
-    let layout = 
-    {
-
-    };
-
-    // Plotly.newPlot("bar", [trace1], layout);
 });
